c31a5efee840ae09388cf6789d06de52fe8bd029
venu211 <venudondapati2008@gmail.com>
2019-06-17 14:04:19 -0500

    add pom dependency and readme file

---
+++ README.md
@@ -0,0 +1,9 @@
+#### JSON2POJO
+
+This utility app will create POJO classes by reading the JSON file using jsonschema2pojo library.
+<br/>
+<br>
+<br>
+
+
+###### Ref: https://dzone.com/articles/converting-json-to-pojos-using-java``
---
+++ src/test/java/com/util/json2pojo/Json2pojoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.util.json2pojo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Json2pojoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
